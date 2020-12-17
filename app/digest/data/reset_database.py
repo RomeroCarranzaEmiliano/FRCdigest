b2063415bfd24b88d067b33b7a2a9ae2c44ec839
RomeroCarranzaEmiliano <emilianoromerocarranza@gmail.com>
2020-12-17 20:00:16 -0300

    reset_database.py advances, database configuration file

--- app/digest/data/reset_database.py
+++ app/digest/data/reset_database.py
@@ -6,36 +6,68 @@
 
 # IMPORTS #########################################################################################
 import sqlite3
+import yaml
+import os
+from static import handler
 ###################################################################################################
 
-# Sensitive data needed
-database_file = 'FRCdigest.db' # <-- this should be done through an enviroment file called secrets.env
+# SQL QUERIES #####################################################################################
+def sql_create_subjects_table():
+	return 'CREATE TABLE Subjects \
+			(id INTEGER, name VARCHAR(100), acronym VARCHAR(10), level INTEGER);'
+
+def sql_create_correlations_table():
+	return 'CREATE TABLE Correlations \
+		(subject_id INTEGER, need_to VARCHAR(10), \
+		correlative_subject_id INTEGER, plan INTEGER, correlative_status_needed VARCHAR(10));'
 
 
-# Connection to the database
-connection = sqlite3.connect(database_file)
+###################################################################################################
 
-# Cursor object
-cursor = connection.cursor()
+def load_config_data():
+	# Load database config file
+	database_config = yaml.safe_load(open('database_config.yaml', 'rt'))
 
-# SQL consults by execution order <-- PK and FK need to be set
-# Creation of the Subjects table
-sql_consult_1 = 'CREATE TABLE Subjects (id INTEGER, name VARCHAR(100), acronym VARCHAR(10), level INTEGER);'
-# Creation of the Correlations table
-sql_consult_2 = 'CREATE TABLE Correlations (subject_id INTEGER, for VARCHAR(10), \
-				correlative_subject_id INTEGER, correlative_status_needed VARCHAR(10));'
-
-# Insertion of default rows (subjects, acronyms and correlations)
-"""
-	this must be done by executing a fuction in another module that returns
-	two vectors of registers, one for subjects and one for correlations
-"""
+	return database_config
 
 
+def main():
+	#
 
-result = cursor.fetchall() # <-- result should be evaluated to handle any error
+	# Load configurations
+	config_data = load_config_data()
 
-print(result)
+	database_path = config_data['database_path']
 
-# close connection
-connection.close(
+	# If database file doesn't exists
+	if not os.path.exists(database_path):
+		print('There is no database created in the path:', database_path)
+		
+		answer = ''
+		while answer not in ['y', 'n', 'yes', 'no']:
+			answer = str(input('Do you want to continue by creating the database? y/n >> '))
+
+		if answer in ['n', 'no']:
+			return
+	else:
+		# Removal of existing database file
+		os.remove(database_path)
+
+	# Connection to the database & creation if it doesn't exists
+	connection = sqlite3.connect(database_path)
+
+	# Cursor object
+	cursor = connection.cursor()
+
+
+	# Execution of queries
+	# -----------------------------------------------------------------------------------
+	cursor.execute(sql_create_subjects_table())
+	cursor.execute(sql_create_correlations_table())
+	# -----------------------------------------------------------------------------------
+
+
+	# Close connection
+	connection.close()
+
+main()
